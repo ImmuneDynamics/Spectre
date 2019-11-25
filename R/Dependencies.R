ffc122b5b3217756068649c88fb13f89d735f2e3
“Thomas Ashhurst” <tomashhurst@gmail.com>
2019-11-25 21:29:05 +1100

    Update Dependencies.R

--- R/Dependencies.R
+++ R/Dependencies.R
@@ -5,9 +5,11 @@
     if(!require('data.table')) {install.packages('data.table', repos = "https://mirror.aarnet.edu.au/pub/CRAN/")}
     if(!require('tidyr')) {install.packages('tidyr', repos = "https://mirror.aarnet.edu.au/pub/CRAN/")}
     if(!require('rstudioapi')) {install.packages('rstudioapi')}
+    if(!require('rtsne')) {install.packages('rtsne')}
 
     ### Load packages
     library('plyr')
     library('data.table')
     library('tidyr')
     library('rstudioapi')
+    library('rtsne')
