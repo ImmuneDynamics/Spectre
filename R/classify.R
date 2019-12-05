96e84ec6c8ff5b63a254b28a1e65f7d70ceb4032
Givanna Putri <ghar1821@uni.sydney.edu.au>
2019-12-05 17:13:43 +1100

    Fix classifier and sample run script

--- R/classify.R
+++ R/classify.R
@@ -1,82 +1,113 @@
+#' normalise.data - ...
+#' 
+#' @usage normalise.data(x, ...)
+#' 
+#' @param data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) to be normalised.
+#' 
+#' Normalise the data to range between 0 and 1.
+#' 
+#' @return Normalised data
+#' 
+#' @export
+normalise.data <- function(data) {
+  norm.data <- (data - min(data))/(max(data)-min(data))
+  return(norm.data)
+}
+
+
 #' train.classifier - ...
 #'
 #' @usage train.classifier(x, ...)
 #'
-#' @param data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) that is the data to be classified.
-#' @param classifier.col NO DEFAULT to KNN. Name of the column in the training data to be used as the training labels
-#' @param label.col NO DEFAULT. Name of the new column that contains classification labels.
-#' @param num.neighbours DEFAULTS to 1. When using a k-nearest neighbour classifier, then this parameter specifies the number of nearest neighbours.
-#' @param training NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) that is the data to be use as training.
+#' @param train.data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) to be used to train classifier. 
+#' Only include columns/features/markers to be used to train the classifier i.e. the ones that are useful in identifying cells (which population it belongs to).
+#' @param label NO DEFAULT. A character vector containing the label (population name) for each cell in the train.data. Must be "CHARACTER" vector.
+#' @param min.num.neighbours DEFAULTS to 1. When using a k-nearest neighbour (KNN) classifier, this parameter specifies the minimum number of nearest neighbours used to train the KNN classifier.
+#' @param max.num.neighbours DEFAULTS to 1. When using a k-nearest neighbour (KNN) classifier, this parameter specifies the maximum number of nearest neighbours used to train the KNN classifier.
+#' @param evaluation.metric DEFAULTS to 'Accuracy'. How do you want the classifier performance to be evaluated? By measuring accuracy (pass "Accuracy") or Cohen Kappa score (pass "Kappa").
+#' @param num.folds DEFAULTS to 10. Number of chunks the training data is to be split into. The classifier will then take 1 chunk for testing the classifier (after it's trained), and use the remaining chunk to train the classifier.
+#' @param num.repeats DEFAULTS to 10. Number of time the classifier will be trained (per number of neighbours). For each repeat, different chunk will be used for testing and training.
 #' 
-#' Train a classifier and return the accuracy of the classifier.
+#' Train a k-nearest neighbour (KNN) classifier.
+#' The classifier will be trained on a number of neighbours, starting from min.num.neighbours, and increased gradually by 1 until max.num.neighbours is reached.
+#' For each number of neighbour, the accuracy of the classifier will be computed.
+#' Data will be normalised to range between 0 and 1 before used in training the classifier.
+#' The normalisation method used is MinMaxScaling method.
 #'
-#' @return The accuracy of the classifier
+#' @return The performance of the classifier on different number of neighbours as well as some description on how the training was performed
+#' i.e. what sampling process is used (how the data is split for training and testing),
+#' the sample sizes (number of data points used for training/testing).
+#' Included as well is the recommended number of neighbours for the data and the reasoning behind why that number of neighbours is the best.
 #'
 #' @export
 
-train.classifier <- function(data, 
-                     classifier.col,
-                     label.col,
-                     num.neighbours = 1){
+
+
+train.classifier <- function(train.data, 
+                             label, 
+                             min.num.neighbours = 1,
+                             max.num.neighbours = 10,
+                             evaluation.metric = 'Accuracy',
+                             num.folds = 10,
+                             num.repeats = 10){
   
-  # split data into training and testing. TODO: convet to 10 fold cross validation
-  ran <- sample(1:nrow(data), 0.9 * nrow(data)) 
+  # normalised the training data
+  normalised.data <- normalise.data(train.data)
   
-  train.data <- data[ran, classifier.col]
-  train.label <- data[ran, label.col]
+  # add the label as a column in normalised data
+  normalised.data['Population'] <- label
   
-  test.data <- data[-ran, classifier.col]
-  test.label <- data[-ran, label.col]
+  # split data into 10 folds and repeat training and testing 10 times
+  # each iteration will train and test on different fold.
+  trControl <- trainControl(
+    method = 'repeatedcv',
+    number = num.folds,
+    repeats = num.repeats
+    )
   
-  # min max normalisation function
-  nor <-function(x) { (x -min(x))/(max(x)-min(x))   }
+  # train the classifier
+  knn.fit <- train(Population ~ .,
+                   method     = "knn",
+                   tuneGrid   = expand.grid(k = min.num.neighbours:max.num.neighbours),
+                   trControl  = trControl,
+                   metric     = evaluation.metric,
+                   data       = normalised.data)
   
-  train.data.norm <- as.data.frame(lapply(train.data[,], nor))
-  test.data.norm <- as.data.frame(lapply(test.data[,], nor))
-  
-  pr <- knn(train.data.norm, test.data.norm, cl=train.label,k=num.neighbours)
-  
-  tab <- table(pr,test.label)
-  
-  ##this function divides the correct predictions by total number of predictions that tell us how accurate teh model is.
-  
-  accuracy <- function(x){sum(diag(x)/(sum(rowSums(x)))) * 100}
-  
-  return(accuracy(tab))
+  return(knn.fit)
 }
 
 #' run.classifier - ...
 #'
 #' @usage run.classifier(x, ...)
 #'
-#' @param training.data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) that is the data to be used to train classifer.
-#' @param training.label NO DEFAULT to KNN. Name of the column in the training data to be used as the training labels.
+#' @param train.data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) to be used to train classifier. 
+#' Only include columns/features/markers to be used to train the classifier i.e. the ones that are useful in identifying cells (which population it belongs to).
+#' @param train.label NO DEFAULT. A character vector containing the label (population name) for each cell in the train.data. Must be "CHARACTER" vector.
 #' @param unlabelled.data NO DEFAULT. A dataframe containing cells (rows) vs features/markers (columns) that is the data classified.
 #' @param num.neighbours DEFAULTS to 1. When using a k-nearest neighbour classifier, then this parameter specifies the number of nearest neighbours.
 #' 
-#' Train and run the classifier.
+#' Train a k-NN classifier on a training data, and use it to classify an unlabelled data. 
+#' Note that for the classifier to work as intended, the unlabelled.data has to be normalised to the range of the train.data.
+#' Note make sure that train.data and unlabelled.data has exactly the same features/markers.
 #'
 #' @return The predicted label for the unlabelled data.
 #'
 #' @export
-run.classifier <- function(training.data, 
-                           training.label,
+run.classifier <- function(train.data, 
+                           train.label,
                            unlabelled.data,
                            num.neighbours = 1){
   
-  # min max normalisation function
-  nor <-function(x) { (x -min(x))/(max(x)-min(x))   }
-  
   # normalised training and unlabelled data together
-  all.data <- rbind(training.data, unlabelled.data)
-  all.data.norm <- as.data.frame(lapply(all.data, nor))
+  all.data <- rbind(train.data, unlabelled.data)
+  all.data.norm <- normalise.data(all.data)
   
-  train.data.norm <- all.data.norm[c(1:nrow(training.data)),]
-  start.test.data <- nrow(training.data) + 1
+  train.data.norm <- all.data.norm[c(1:nrow(train.data)),]
+  start.test.data <- nrow(train.data) + 1
   
   test.data.norm <- all.data.norm[c(start.test.data:nrow(all.data.norm)),]
   
-  pr <- knn(train.data.norm, test.data.norm, cl=training.label,k=num.neighbours)
+  pr <- knn(train.data.norm, test.data.norm, cl=train.label,k=num.neighbours)
   
   return(pr)
 }
