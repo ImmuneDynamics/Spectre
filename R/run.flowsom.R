7a68ccd7a6b2ca62d167f9e4e00e25b6ca758d42
Felix Marsh-Wakefield <felixmarsh-wakefield@192-168-1-5.tpgi.com.au>
2019-12-11 17:33:42 +1100

    Update run.flowsom
    Added optional calculation of metaclusters. Removed unused comments.

--- R/run.flowsom.R
+++ R/run.flowsom.R
@@ -8,6 +8,7 @@
 #' @param xdim Numeric. Number of first level clusters across the x-axis. xdim x ydim = total number of first level FlowSOM clusters. DEFAULT = 10.
 #' @param ydim Numeric. Number of first level clusters across the y-axis. xdim x ydim = total number of first level FlowSOM clusters. DEFAULT = 10.
 #' @param clust.seed Numeric. Clustering seed for reproducibility. DEFAULT = 42
+#' @param create.metacluster Option to not create metaclusters (and only have clusters). DEFAULT = TRUE.
 #' @param meta.seed Numeric. Metaclustering seed for reproducibility. DEFAULT = 42.
 #' @param clust.name Character. Name of the resulting 'cluster' parameter. DEFAULT = "FlowSOM_cluster".
 #' @param meta.clust.name Character. Name of the resulting 'metacluster' parameter. DEFAULT = "FlowSOM_metacluster".
@@ -22,6 +23,7 @@ run.flowsom <- function(x,
                         xdim = 10,
                         ydim = 10,
                         clust.seed = 42,
+                        create.metacluster = TRUE,
                         meta.seed = 42,
                         clust.name = "FlowSOM_cluster",
                         meta.clust.name = "FlowSOM_metacluster"){
@@ -59,17 +61,12 @@ run.flowsom <- function(x,
   ## Create FCS file metadata - column names with descriptions
   metadata <- data.frame(name=dimnames(x)[[2]], desc=paste('column',dimnames(x)[[2]],'from dataset'))
 
-  ## Create FCS file metadata - ranges, min, and max settings -- by default, they are commented out (adjust ranges manually in FlowJo)
-      #metadata$range <- apply(apply(data,2,range),2,diff) # throws an error because of word entry -- hopefully is ok
-      #metadata$minRange <- apply(data,2,min)
-      #metadata$maxRange <- apply(data,2,max)
-
   ## Create flowframe with data
   x.ff <- new("flowFrame",
                  exprs=as.matrix(x), # in order to create a flow frame, data needs to be read as matrix
-                 parameters=AnnotatedDataFrame(metadata))
+                 parameters=Biobase::AnnotatedDataFrame(metadata))
 
-  head(exprs(x.ff))
+  head(flowCore::exprs(x.ff))
 
   x_FlowSOM <- x.ff
 
@@ -91,19 +88,6 @@ run.flowsom <- function(x,
 
   FlowSOM_out <- FlowSOM::BuildMST(FlowSOM_out)
 
-  ### some warnings will be returned because of the 'SampleName' and 'GroupName' entries
-
-  ## Optional visualization
-
-  #FlowSOM::PlotStars(FlowSOM_out) # won't plot names if arial font problems still exists, # if 'sample name' is in there, can't plot
-
-  #set.seed(42)
-  #FlowSOM::PlotStars(FlowSOM_out,view="tSNE")
-
-  #print(colnames(FlowSOM_out$map$medianValues))
-  #FlowSOM::PlotMarker(FlowSOM_out,"BUV395.CD11b")
-  #FlowSOM::PlotNumbers(UpdateNodeSize(FlowSOM_out,reset=TRUE))
-
   ## extract cluster labels (pre meta-clustering) from output object
   labels_pre <- FlowSOM_out$map$mapping[, 1]
   labels_pre
@@ -111,33 +95,28 @@ run.flowsom <- function(x,
   nrow(x)
 
   flowsom.res.original <- labels_pre
-
-  ## run meta-clustering
-  FlowSOM_out_meta <- FlowSOM::metaClustering_consensus(FlowSOM_out$map$codes, k = meta.k, seed = meta.seed)
-
-  # note: In the PREVIOUS version of FlowSOM, the meta-clustering function
-  # FlowSOM::metaClustering_consensus() does not pass along the seed argument
-  # correctly, so results are not reproducible. We use the internal function
-  # ConsensusClusterPlus::ConsensusClusterPlus() to get around this.
-
-  # seed <- 1234
-  # out <- ConsensusClusterPlus::ConsensusClusterPlus(t(out$map$codes), maxK = FlowSOM_kvalue, seed = seed)
-  # out <- out[[FlowSOM_kvalue]]$consensusClass
-
-  # However, this
-  # HAS BEEN fixed in the next update of FlowSOM (version 1.5); then the following
-  # (simpler) code can be used instead:
-
-  ## Optional visualisation
-  # FlowSOM::PlotMarker(FlowSOM_out,"BUV395.CD11b", backgroundValues = as.factor(FlowSOM_out_meta))
-
-
-  ## extract META (?) cluster labels from output object
-  labels <- FlowSOM_out_meta[labels_pre]
-
-  ## summary of cluster sizes and number of clusters
-  table(labels)
-  length(table(labels))
+  
+  if (create.metacluster == TRUE) {
+    ## run meta-clustering
+    FlowSOM_out_meta <- FlowSOM::metaClustering_consensus(FlowSOM_out$map$codes, k = meta.k, seed = meta.seed)
+    
+    ## extract META (?) cluster labels from output object
+    labels <- FlowSOM_out_meta[labels_pre]
+    
+    ## summary of cluster sizes and number of clusters
+    table(labels)
+    length(table(labels))
+    
+    ## save META cluster labels
+    flowsom.res.meta <- data.frame("labels" = labels)
+    colnames(flowsom.res.meta)[grepl('labels',colnames(flowsom.res.meta))] <- meta.clust.name
+    
+    dim(x)
+    dim(flowsom.res.meta)
+    head(flowsom.res.meta)
+    
+    assign("flowsom.res.meta", flowsom.res.meta, envir = globalenv())
+  }
 
   ## save ORIGINAL cluster labels
   flowsom.res.original <- data.frame("labels_pre" = labels_pre)
@@ -147,19 +126,8 @@ run.flowsom <- function(x,
   dim(flowsom.res.original)
   head(flowsom.res.original)
 
-  ## save META cluster labels
-  flowsom.res.meta <- data.frame("labels" = labels)
-  colnames(flowsom.res.meta)[grepl('labels',colnames(flowsom.res.meta))] <- meta.clust.name
-
-  dim(x)
-  dim(flowsom.res.meta)
-  head(flowsom.res.meta)
-
   assign("flowsom.res.original", flowsom.res.original, envir = globalenv())
-  assign("flowsom.res.meta", flowsom.res.meta, envir = globalenv())
 
 }
-
-
 
 
