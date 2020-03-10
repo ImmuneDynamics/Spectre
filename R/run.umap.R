a09ad3973d87f24235a5f2be37197462d2e9dcfb
Felix Marsh-Wakefield <felixmarsh-wakefield@192-168-1-9.tpgi.com.au>
2020-03-11 06:40:15 +1100

    Updated run.umap
    Added option to change UMAP column names.

--- R/run.umap.R
+++ R/run.umap.R
@@ -2,17 +2,21 @@
 #'
 #' @param dat NO DEFAULT. Input data.table or data.frame.
 #' @param use.cols NO DEFAULT. Vector of column names or numbers for clustering.
+#' @param umap.x.name DEFAULT = "UMAP_X". Character. Name of UMAP x-axis.
+#' @param umap.y.name DEFAULT = "UMAP_Y". Character. Name of UMAP y-axis.
 #' @param umap.seed DEFAULT = 42. Numeric. Seed value for reproducibility.
 #' @param suffix DEFAULT = blank.
 #'
 #' This function runs UMAP on a dataframe with cells (rows) vs markers (columns), and returns 'res' with result columns.
 #' 
-#' @usage run.umap(dat, use.cols, umap.seed, suffix, ...)
+#' @usage run.umap(dat, use.cols, umap.x.name, umap.y.name, umap.seed, suffix, ...)
 #'
 #' @export
 
 run.umap <- function(dat,
                      use.cols,
+                     umap.x.name = "UMAP_X",
+                     umap.y.name = "UMAP_Y",
                      umap.seed = 42,
                      suffix = NULL
                      )
@@ -37,8 +41,8 @@ run.umap <- function(dat,
   head(umap.res)
 
   if(is.null(suffix) == TRUE){ # if suffix = NULL
-    names(umap.res)[names(umap.res) == "V1"] <- paste0("UMAP_X")
-    names(umap.res)[names(umap.res) == "V2"] <- paste0("UMAP_Y")
+    names(umap.res)[names(umap.res) == "V1"] <- umap.x.name
+    names(umap.res)[names(umap.res) == "V2"] <- umap.y.name
   }
 
   if(is.null(suffix) == FALSE){ # if suffix = anything other than NULL
