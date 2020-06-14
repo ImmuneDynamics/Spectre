0d394a8f2604539dca72a522824bafc1f9f970d5
Felix Marsh-Wakefield <felixmarsh-wakefield@192-168-1-7.tpgi.com.au>
2020-06-14 13:44:46 +1000

    Updated do.subsample
    Standardised description.

--- R/do.subsample.R
+++ R/do.subsample.R
@@ -1,6 +1,8 @@
-#' do.subsample - ...
-#'
-#' @usage do.subsample(dat, method, samp.col, targets, seed)
+#' Subsample data
+#' 
+#' Method to subsample data.
+#' Can subsample by randomly selecting a desired number of cells from all samples (method = "random"), subsample by specifying the exact number of cells for each sample (method= "per.sample"), or by subsampling the same number of cells from each sample based on the sample with the lowest count (method = "min.per.sample").
+#' Useful to decrease total cells for generating dimensionality reduction plots (tSNE/UMAP).
 #'
 #' @param dat NO DEFAULT. Input dataframe with cells (rows) vs markers (columns).
 #' @param method NO DEFAULT. Character. Can be 'random' (downsampling single number from merged data), 'per.sample' (specifying number of cells each sample contributes) or 'min.per.sample' (each sample contributes the same amount of data based on sample with lowest count).
@@ -8,24 +10,30 @@
 #' @param targets NO DEFAULT. List of downsample targets (if random, then absolute target; if per.sample, then targets per sample, must be in the same order as the unique sample names appear in the data frame or data table; can leave blank if min.per.sample).
 #' @param seed DEFAULT = 42. Numeric. Seed for reproducibility.
 #'
-#' @return Returns a subsampled data.table
-#'
-#' This function facilitates downsampling of a dataframe.
-#'
+#' @usage do.subsample(dat, method, samp.col, targets, seed)
+#' 
+#' @examples
+#' Spectre::do.subsample(dat = Spectre::demo.start,
+#'                       method = "per.sample",
+#'                       samp.col = "FileName",
+#'                       targets = c(rep(1000,12)))
+#' 
+#' @author
+#' Thomas Ashhurst, \email{thomas.ashhurst@@sydney.edu.au}
+#' Felix Marsh-Wakefield, \email{felix.marsh-wakefield@@sydney.edu.au}
 #' @export
 
 do.subsample <- function(dat,
-                         method, # random, per.sample
+                         method, # random, per.sample, min.per.sample
                          samp.col, # column than determines sample names
                          targets, # c(1000, 1500, ...)
                          seed = 42){
-
-  ## Test data
-      #dat <- cell.dat
-      #method <- "per.sample"
-      #samp.col <- "FileName"
-      #targets <- c(rep(1000, 12))
-      #seed <- 42
+  
+  ## Check that necessary packages are installed
+  if(!is.element('Spectre', installed.packages()[,1])) stop('Spectre is required but not installed')
+  
+  ## Require packages
+  require(Spectre)
 
   ## IF random # WORKS
   if(method == "random"){
@@ -41,7 +49,7 @@ do.subsample <- function(dat,
 
     # Create list of unique sample names
     sample.list <- unique(dat[samp.col])
-    sample.list <-sample.list[,1]
+    sample.list <- sample.list[,1]
     sample.list
 
     # Create res data.frame
@@ -68,7 +76,7 @@ do.subsample <- function(dat,
   if(method == "min.per.sample") {
     # Create list of unique sample names
     sample.list <- unique(dat[samp.col])
-    sample.list <-sample.list[,1]
+    sample.list <- sample.list[,1]
     sample.list
 
     #nrow.check = list()
