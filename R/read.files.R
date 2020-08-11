be41fbe4e8170a2b32b65f9843c28595b47e4e79
Felix Marsh-Wakefield <felixmarsh-wakefield@192-168-1-2.tpgi.com.au>
2020-08-11 15:45:34 +1000

    Update read.files
    'FlowCore' will now only be opened if filetype = .fcs

--- R/read.files.R
+++ R/read.files.R
@@ -30,20 +30,13 @@ read.files <- function(file.loc = getwd(),
                        header = TRUE)
 {
 
-    ## For testing
-            #file.loc <- "/Users/thomasa/Google Drive File Stream/My Drive/_Sydney Cytometry/Libraries (synced)/GitHub/Public github/Spectre/worked_examples/CAPX_workflow/"
-            #file.type <- ".csv"
-            #do.embed.file.names = TRUE
-
     ## Check that necessary packages are installed
         if(!is.element('Spectre', installed.packages()[,1])) stop('Spectre is required but not installed')
         if(!is.element('data.table', installed.packages()[,1])) stop('data.table is required but not installed')
-        if(!is.element('flowCore', installed.packages()[,1])) stop('flowCore is required but not installed')
 
     ## Require packages
         require(Spectre)
         require(data.table)
-        require(flowCore)
 
     ## Initial warnings
         wd <- setwd(file.loc)
@@ -79,6 +72,9 @@ read.files <- function(file.loc = getwd(),
 
     ## For reading FCS files
         if(file.type == ".fcs"){
+          if(!is.element('flowCore', installed.packages()[,1])) stop('flowCore is required but not installed')
+          require(flowCore)
+          
           file.names <- list.files(path=file.loc, pattern = file.type)
 
           for (file in file.names) { # Loop to read files into the list
@@ -109,26 +105,7 @@ read.files <- function(file.loc = getwd(),
           for (a in all.file.names) {data.list[[a]]$FileName <- a} # Inserting names doesn't stop the rest working, just messes with some auto visual stuff
           for (i in all.file.nums) {data.list[[i]]$FileNo <- i}
 
-          # assign("all.file.names", all.file.names, envir = globalenv())
-          # assign("all.file.nums", all.file.nums, envir = globalenv())
         }
-
-    ## Creating column and row counts, as well as name checks
-
-        # for(i in c(1:(length(data.list)))){ncol.check[[i]] <- length(names(data.list[[i]]))} # creates a list of the number of columns in each sample
-        # for(i in c(1:(length(data.list)))){colName.check[[i]] <- names(data.list[[i]])}
-        # name.table <- data.frame(matrix(unlist(colName.check), nrow = length(data.list), byrow = T))
-        # for(i in c(1:(length(data.list)))){nrow.check[[i]] <- nrow(data.list[[i]])}
-        #
-        # ncol.check <- as.matrix(ncol.check)
-        # nrow.check <- as.matrix(nrow.check)
-
-    ## Save relevant values
-        # assign("name.table", name.table, envir = globalenv())
-        # assign("ncol.check", ncol.check, envir = globalenv())
-        # assign("nrow.check", nrow.check, envir = globalenv())
-
-        #assign("colName.check", colName.check, envir = globalenv())
 
     ## Return result and result message
         return(data.list)
