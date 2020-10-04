1160b951f7255643fca75b755630194a470777cc
Felix Marsh-Wakefield <felixmarsh-wakefield@192-168-1-2.tpgi.com.au>
2020-10-04 13:07:02 +1100

    Updated run.tsne
    Was running into problems (data.table again...), but it should be good to go!

--- R/run.tsne.R
+++ R/run.tsne.R
@@ -74,7 +74,7 @@ run.tsne <- function(dat,
   set.seed(tsne.seed)
 
   ## Run tSNE
-  tsne_out <- Rtsne::Rtsne(as.matrix(dat[use.cols]),
+  tsne_out <- Rtsne::Rtsne(as.matrix(dat[, use.cols, with = FALSE]),
         dims = dims,
         initial_dims = initial_dims,
         perplexity = perplexity,
