adc2d38113d026fe3da07afdeecfed43c32c3553
Thomas Ashhurst <tomashhurst@users.noreply.github.com>
2020-12-10 16:37:54 +1100

    Updates

--- R/do.mn.R
+++ R/do.mn.R
@@ -1,16 +1,17 @@
-#' do.mn - Multipurpose function for arcsinh transformation, quantile normalisation, noise reduction, and normalisation
+#' do.mn - Multipurpose function for arcsinh transformation, outlier management, and quantile-based re-scaling
 #'
-#' This function facilitates multiple data transformation functions in a single process, including arcsinh transformation, noise reduction, and normalisation
+#' This function facilitates multiple data transformation functions in a single process, including arcsinh transformation, outlier management, and quantile-based re-scaling.
 #'
 #' @usage do.mn(dat, use.cols)
 #'
 #' @param dat NO DEFAULT. A data.table
 #' @param use.cols NO DEFAULT. Vector of column names that you wish to transform
 #' @param asinh.cf DEFAULT = 15. The co-factor to use for arcsinh transformation
-#' @param norm DEFAULT = c(0,1). New MIN and MAX values for each channel, as c(MIN, MAX). 
-#' @param lower.threshold DEFAULT = 0. Values below this number will be converted to this number.
-#' @param upper.Q DEFAULT = 0.995. To not utilise the upper quantile function, simply use upper.Q = NULL or upper.Q = 1. When normalising, the new 'maximum' value (defined in the 'norm' argument, typically '1') will be set to this quantile target (for upper.Q = 0.995, this is the 99.5th percentile). As such, any values above the 99.5th percentile will increase above this new maximum value proportionally. Any target between 0 and 1 can be set. 
+#' @param lower.Q DEFAULT = 0.001. Quantile target for values at the lower threshold
+#' @param upper.Q DEFAULT = 0.995. Quantile target for rescaling, such that the data will be scaled between 0 (the minimum value after clipping) and 1 (upper.Q), where values above the upper.Q will increase above 1 proportionatly.
+#' @param lower.func DEFAULT = 'clip'. Determine what is done to values below the lower.Q threshold. 'clip' means values below the lower.Q quantile threshold will be clipped to that value. NULL will skip.
 #' @param append.name DEFAULT = '_transf'. Text to be appended to resultant column name
+#' @param save.info DEFAULT = TRUE. Save's the transformation details to a .txt file.
 #' @param info.dir DEFAULT = getwd(). Directory address to save a .txt. file with the settings used for the MRN function.
 #' 
 #' @return Returns a data.table with transformed data added as new columns.
@@ -30,10 +31,11 @@ do.mn <- function(dat,
                   use.cols,
                    
                   asinh.cf = 15,
-                  norm = c(0,1),
                   
-                  lower.threshold = 0,
+                  lower.Q = 0.001,
                   upper.Q = 0.995,
+                  
+                  lower.func = 'clip', 
                   
                   append.name = '_transf',
                   save.info = TRUE,
@@ -46,20 +48,30 @@ do.mn <- function(dat,
       library(data.table)
   
   ### Test data
-      
+
       # dat <- Spectre::demo.start
       # use.cols <- names(dat)[c(2:10)]
       # 
       # asinh.cf <- 1000
       # norm = c(0,1)
       # 
-      # lower.threshold = 0
+      # lower.Q = 0.005
       # upper.Q = 0.995
+      # 
+      # lower.func = 'clip'
       # 
       # save.info = TRUE
       # append.name = '_transf'
       # info.dir = getwd()
+
+  ### Some settings and tests
+
+      norm = c(0,1) # default re-scaling to 0 and 1
   
+      if(lower.squish >= upper.Q){
+        stop(paste0("Qs[1] is greater than Qs[2]"))
+      }
+      
   ### Extract data
   
       value <- dat[,use.cols,with = FALSE]
@@ -82,72 +94,122 @@ do.mn <- function(dat,
         message("Arcsinh transformation complete")
       }
  
-  ### Lower threshold
-      
-      if(!is.null(lower.threshold)){
-        for(a in use.cols){
+  ### Lower Q
+
+      if(!is.null(lower.Q)){
+        
+        if(!is.null(lower.func)){
+        
+            if(lower.func == 'clip'){
+              for(a in use.cols){
+                # a <- use.cols[[2]]
+                
+                temp <- value[,a,with = FALSE]
+                b <- quantile(temp[[1]], lower.Q)
+                
+                temp[temp[[a]] < b,] <- b
+                value[,a] <- temp
+                
+                rm(a)
+                rm(b)
+                rm(temp)
+              }
+              message("Lower quantile clipping is complete")
+            }
           
-          b <- lower.threshold
-          temp <- value[,a,with = FALSE]
-          temp[temp[[a]] < b,] <- b
-          value[,a] <- temp
-          
-          rm(a)
-          rm(b)
-          rm(temp)
         }
-        message("Lower value thresholding is complete")
       }
+
+  # ### Upper squish
+      #     
+      #     if(!is.null(upper.squish)){
+      #       for(a in use.cols){
+      #         
+      #         b <- upper.squish
+      #         temp <- value[,a,with = FALSE]
+      #         temp[temp[[a]] > b,] <- b
+      #         value[,a] <- temp
+      #         
+      #         rm(a)
+      #         rm(b)
+      #         rm(temp)
+      #       }
+      #       message("Upper squish clipping is complete")
+      #     }
       
-  ### Normalisation, including upper Q target
+  ### Rescale between min and upper Q as 1
       
       if(!is.null(norm)){
         
         ## No upper Q (upper Q is NULL)
-        if(is.null(upper.Q)){
-          norm.fun <- function(x) {(x - min(x))/(max(x)-min(x)) * (norm[2] - norm[1]) + norm[1]}
-          value <- as.data.table(lapply(value, norm.fun)) # by default, removes the names of each row
-          
-          message("Normalisation between ", norm[1], " and ", norm[2], " complete")
-        }
+            
+            if(is.null(upper.Q)){
+              norm.fun <- function(x) {(x - min(x))/(max(x)-min(x)) * (norm[2] - norm[1]) + norm[1]}
+              value <- as.data.table(lapply(value, norm.fun)) # by default, removes the names of each row
+              
+              message("Normalisation between ", norm[1], " and ", norm[2], " complete")
+            }
         
-        ## With upper Q (upper Q is NOT NULL)
-        if(!is.null(upper.Q)){
-          for(a in use.cols){
-            # a <- use.cols[[1]]
-            
-            A <- min(value[[a]])
-            B <- quantile(value[[a]], probs = upper.Q)
-            C <- max(value[[a]])
-            
-            pt.1 <- (B)-(A) # range from in to upper.Q
-            pt.2 <- pt.1 + (C-B) 
-            mx <- pt.2/pt.1
-            mx <- mx * norm[2]
-            
-            temp <- value[, a, with = FALSE]
-            temp <- as.data.table(temp)
-            names(temp) <- a
-
-            # norm.fun <- function(x) {(x - min(x))/(max(x)-min(x)) * (mx - norm[1]) + norm[1]}
-            # temp <- as.data.table(norm.fun(temp)) # by default, removes the names of each row
-            
-            res <- do.normalise(temp, a, new.min = norm[1], new.max = mx)
-            res <- res[[paste0(a, "_norm")]]
-            
-            value[[a]] <- res
-
-            rm(a)
-            rm(A)
-            rm(B)
-            rm(C)
-            rm(temp)
-            rm(res)
-            rm(pt.1)
-            rm(pt.2)
-          }
-          message("Normalisation between ", norm[1], " and ", norm[2], " complete, with upper.Q = ", upper.Q)
-        }
+        ## With lower and upper Q (upper Q is NOT NULL)
+        
+            if(!is.null(Qs)){
+                
+                  for(a in use.cols){
+                    # a <- use.cols[[2]]
+                    
+                    A <- min(value[[a]])
+                    B1 <- quantile(value[[a]], probs = Qs[1])
+                    B2 <- quantile(value[[a]], probs = Qs[2])
+                    C <- max(value[[a]])
+    
+                    ## Upper Q calculations
+                    
+                        pt.1 <- (B2)-(A)        # range from min to upper.Q
+                        pt.2 <- pt.1 + (C-B2)   # range from min to upper.Q + range from upper.Q to max (total range)
+                        mx <- pt.2/pt.1         # ratio of [total range] over [min to upper.Q]
+                        mx <- mx * norm[2]      # ratio x new target value
+    
+                    ## Lower Q calculations
+                        
+                        # mn <- ((A) - (B1)) * (mx / C)
+                        # mn
+    
+                        # pt.1 <- (C)-(B1)        # range from max to lower.Q
+                        # pt.2 <- pt.1 + (B1)-(A) # range from max to lower.Q + range from lower.Q to min (total range)
+                        # 
+                        # x - B1 # range from lowerQ to upper Q (=1)
+                        # B1 - A # range from lowerQ to min
+                        # 
+                        # mn <- (A) / (pt.1)
+                        # 
+                        # # mn <- pt.2 / (norm[1] - ((B1) - (A)))
+                    
+                    temp <- value[, a, with = FALSE]
+                    temp <- as.data.table(temp)
+                    names(temp) <- a
+        
+                    # norm.fun <- function(x) {(x - min(x))/(max(x)-min(x)) * (mx - norm[1]) + norm[1]}
+                    # temp <- as.data.table(norm.fun(temp)) # by default, removes the names of each row
+                    
+                    res <- do.normalise(temp, a, new.min = norm[1], new.max = mx)
+                    res <- res[[paste0(a, "_norm")]]
+                    
+                    value[[a]] <- res
+        
+                    rm(a)
+                    rm(A)
+                    rm(B1)
+                    rm(B2)
+                    rm(C)
+                    rm(temp)
+                    rm(res)
+                    rm(pt.1)
+                    rm(pt.2)
+                  }
+                
+              message("Re-scaling between ", norm[1], " and ", norm[2], " complete, with lower.Q = ", Qs[1], " and upper.Q = ", Qs[2])
+            }
+        
       }
       
   ### Wrap up
@@ -164,9 +226,9 @@ do.mn <- function(dat,
                                 "----------------",
                                 paste0("Performed on columns:", use.cols),
                                 paste0("Arcsinh co-factor = ", asinh.cf),
-                                paste0("Low threshold = ", lower.threshold),
+                                paste0("Lower Q (squish) = ", lower.Q),
+                                paste0("Upper Q (re-scale) = ", upper.Q),
                                 paste0("Normalise between = ", norm[1], ' and ', norm[2]),
-                                paste0("Upper Q = ", upper.Q),
                                 paste0("Appended name = ", append.name)
         ))
         
---
+++ tests/function_tests/test_fitsne.R
@@ -0,0 +1,68 @@
+
+library('Spectre')
+package.check()
+package.load()
+
+cell.dat <- Spectre::demo.start
+as.matrix(names(cell.dat))
+use.cols <- names(cell.dat)[c(2:10)]
+
+cell.dat.1 <- do.asinh(cell.dat, use.cols, cofactor = 1000)
+cell.dat.2 <- do.mn(cell.dat, use.cols, asinh.cf = 500, lower.threshold = -2)
+
+make.colour.plot(cell.dat.1, 'CD3_asinh', 'CD4_asinh', filename = 'for test.png')
+
+
+
+
+z.dat <- do.zscore(cell.dat.1, paste0(use.cols, '_asinh'))
+z.dat
+
+make.colour.plot(z.dat, 'CD11b_asinh', 'CD45_asinh')
+make.colour.plot(z.dat, 'CD3_asinh', 'CD4_asinh')
+
+make.colour.plot(cell.dat.2, 'CD3_transf', 'CD4_transf')
+
+
+
+
+
+
+
+
+dat.dt <- as.data.table(Spectre::demo.clustered)
+dat.df <- as.data.frame(Spectre::demo.clustered)
+dat.mt <- as.matrix(Spectre::demo.clustered)
+
+object.size(dat.dt) / 1024 / 1024
+object.size(dat.df) / 1024 / 1024
+object.size(dat.mt) / 1024 / 1024
+
+
+x <- dat.df[1:100,]
+
+
+
+
+dat.transf <- do.mn(cell.dat, use.cols, asinh.cf = 1000)
+dat.transf
+
+
+quantile(cell.dat.1$CD3_asinh, 0.005)
+
+make.colour.plot(cell.dat.1, 'CD3_asinh', 'CD4_asinh', 'CD3_asinh')
+make.colour.plot(dat.transf, 'CD3_transf', 'CD4_transf', 'CD3_transf')
+make.colour.plot(dat.transf, 'CD11b_transf', 'CD45_transf')
+
+dat.transf <- run.flowsom(dat.transf, paste0(use.cols, '_transf'))
+dat.transf <- run.fitsne(dat.transf, paste0(use.cols, '_transf'))
+
+make.colour.plot(dat.transf, 'FItSNE_X', 'FItSNE_Y', 'FlowSOM_metacluster', 'factor', add.label = TRUE)
+
+
+
+
+
+
+
+
