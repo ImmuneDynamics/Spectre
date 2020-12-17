55c13b58fccd28dc98e8f814c1e57568a9dffc39
Givanna Putri <ghar1821@uni.sydney.edu.au>
2020-12-18 09:22:01 +1100

    update logicle transformation

--- tests/test_logicle.R
+++ tests/test_logicle.R
@@ -2,15 +2,41 @@ library(Spectre)
 
 dat = demo.asinh
 use.cols = names(dat)[c(2:10)]
-dat_lgcl1 = do.logicle(dat, use.cols = use.cols)
-dat_lgcl2 = do.logicle(dat, use.cols = use.cols, w = NULL)
+dat_lgcl_auto = do.logicle(dat, use.cols = use.cols)
 
-setwd("~/Documents/phd/workdir/")
+dat_lgcl_custom = do.logicle(dat, use.cols = use.cols, 
+                             auto.infer.function = FALSE,
+                             linearisation.width = 0.1)
 
-make.colour.plot(dat_lgcl1,
+dat_lgcl_custom = do.logicle(dat, use.cols = use.cols, 
+                             auto.infer.function = FALSE,
+                             max.scale.val = 10000)
+
+dat_lgcl_custom = do.logicle(dat, use.cols = use.cols, 
+                             auto.infer.function = FALSE,
+                             full.transform.width = 5)
+
+dat_lgcl_custom = do.logicle(dat, use.cols = use.cols, 
+                             auto.infer.function = FALSE,
+                             additional.negative.range = 2)
+
+dat_lgcl_custom = do.logicle(dat, use.cols = use.cols, 
+                             auto.infer.function = FALSE,
+                             linearisation.width = 0.1,
+                             max.scale.val = 10000,
+                             full.transform.width = 5,
+                             additional.negative.range = 2)
+
+make.colour.plot(dat_lgcl_custom,
                  x.axis = 'Ly6C_logicle',
-                 y.axis = 'CD4_logicle')
+                 y.axis = 'CD4_logicle',
+                 save.to.disk = FALSE)
+make.colour.plot(dat_lgcl_auto,
+                 x.axis = 'Ly6C_logicle',
+                 y.axis = 'CD4_logicle',
+                 save.to.disk = FALSE)
 
+# For comparison purposes
 make.colour.plot(dat_lgcl1,
                  x.axis = 'Ly6C',
                  y.axis = 'CD4',
