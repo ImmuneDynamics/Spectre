10e53ddb87e8aa6dc0d23e93d3d79eb723e586c8
Thomas Ashhurst <tomashhurst@users.noreply.github.com>
2022-10-29 10:37:57 +1100

    Update run.rpca.R

--- R/run.rpca.R
+++ R/run.rpca.R
@@ -45,113 +45,129 @@ run.rpca <- function(dat,
   # }
   
   ### Split data by batch/dataset and create Seurat objects
-  
-  message('Running rPCA')
-  message(' -- setting up data')
-  
-  dat$CELLBARCODE <- c(1:nrow(dat))
-  
-  batches <- unique(dat[[batch.col]])
-  batches
-  
-  org.list <- list()
-  res.list <- list()
-  
-  for(i in batches){
-    # i <- batches[[1]]
-    
-    message(paste0('    ...', i))
-    
-    rm(rws)
-    rws <- dat[[batch.col]] == i
-    
-    rw.org <- dat[rws, 'CELLBARCODE', with = FALSE]
-    org.list[[i]] <- rw.org
-    
-    dat.batch <- dat[rws,..use.cols]
-    
-    counts <- as.matrix(dat.batch)
-    counts <- Matrix::Matrix(counts, sparse = TRUE)
-    rownames(counts) <- paste0('Cell-', i, '-', c(1:nrow(counts)))
-    counts <- t(counts)
-    
-    srt <- CreateSeuratObject(counts = counts, assay = 'cyto')
-    srt <- FindVariableFeatures(srt, selection.method = "vst", nfeatures = length(use.cols))
-    res.list[[i]] <- srt
-  }
-  
-  org.list
-  res.list
+      
+      message('Running rPCA')
+      message(' -- setting up data (1/5)')
+      
+      dat.start <- dat
+      
+      new.names <- paste0('Col', c(1:length(raw.cols)))
+      match <- data.table('Start' = raw.cols, 'New' = new.names)
+      match
+      
+      dat <- dat[,c(batch.col, raw.cols), with = FALSE]
+      names(dat)[c(2:length(names(dat)))] <- new.names
+      dat
+      
+      dat$CELLBARCODE <- c(1:nrow(dat))
+      
+      batches <- unique(dat[[batch.col]])
+      batches
+      
+      org.list <- list()
+      res.list <- list()
+      
+      for(i in batches){
+        # i <- batches[[1]]
+        
+        message(paste0('    ...', i))
+        
+        rws <- dat[[batch.col]] == i
+        
+        rw.org <- dat[rws, 'CELLBARCODE', with = FALSE]
+        org.list[[i]] <- rw.org
+        
+        dat.batch <- dat[rws,..new.names]
+        
+        counts <- as.matrix(dat.batch)
+        counts <- Matrix::Matrix(counts, sparse = TRUE)
+        rownames(counts) <- paste0('Cell-', i, '-', c(1:nrow(counts)))
+        counts <- t(counts)
+        
+        srt <- CreateSeuratObject(counts = counts, assay = 'cyto')
+        srt <- FindVariableFeatures(srt, selection.method = "vst", nfeatures = length(use.cols))
+        res.list[[i]] <- srt
+        
+        rm(rws)
+      }
+      
+      org.list
+      res.list
+      
+      rm(dat)
+      gc()
   
   ### Select integration features, scale data, and run PCA
-  
-  message(' -- performing scaling and PCA')
-  
-  features <- SelectIntegrationFeatures(object.list = res.list)
-  features
-  
-  for(i in batches){
-    
-    message(paste0('    ...', i))
-    
-    res.list[[i]] <- ScaleData(res.list[[i]], features = features, verbose = FALSE, assay = 'cyto')
-    res.list[[i]] <- RunPCA(res.list[[i]], features = features, verbose = FALSE, assay = 'cyto')
-    
-  }
+      
+      message(' -- performing scaling and PCA (1/2)')
+      
+      features <- SelectIntegrationFeatures(object.list = res.list)
+      features
+      
+      for(i in batches){
+        
+        message(paste0('    ...', i))
+        
+        res.list[[i]] <- ScaleData(res.list[[i]], features = features, verbose = FALSE, assay = 'cyto')
+        res.list[[i]] <- RunPCA(res.list[[i]], features = features, verbose = FALSE, assay = 'cyto')
+        
+      }
   
   ### Find integration anchors across datasets
-  
-  message(' -- finding integration anchors')
-  
-  
-  if(is.null(reference)){
-    immune.anchors <- FindIntegrationAnchors(object.list = res.list, 
-                                             anchor.features = features, 
-                                             dims = 1:(length(use.cols)-1), 
-                                             k.anchor = k.anchor,
-                                             reduction = 'rpca')
-    
-    immune.anchors
-    
-  }
-  
-  if(!is.null(reference)){
-    immune.anchors <- FindIntegrationAnchors(object.list = res.list, 
-                                             anchor.features = features, 
-                                             dims = 1:(length(use.cols)-1), 
-                                             k.anchor = k.anchor,
-                                             reduction = 'rpca', 
-                                             reference = which(names(res.list) == reference))
-    
-    immune.anchors
-  }
+      
+      message(' -- finding integration anchors (1/3)')
+      
+      
+      if(is.null(reference)){
+        immune.anchors <- FindIntegrationAnchors(object.list = res.list, 
+                                                 anchor.features = features, 
+                                                 dims = 1:(length(features)-1), 
+                                                 k.anchor = k.anchor,
+                                                 reduction = 'rpca')
+        
+        immune.anchors
+        
+      }
+      
+      if(!is.null(reference)){
+        immune.anchors <- FindIntegrationAnchors(object.list = res.list, 
+                                                 anchor.features = features, 
+                                                 dims = 1:(length(features)-1), 
+                                                 k.anchor = k.anchor,
+                                                 reduction = 'rpca', 
+                                                 reference = which(names(res.list) == reference))
+        
+        immune.anchors
+      }
   
   ### Integrate the data
   
-  message(' -- integrating data')
-  
-  immune.combined <- IntegrateData(anchorset = immune.anchors, dims = 1:(length(use.cols)-1))
-  DefaultAssay(immune.combined) <- "integrated"
+      message(' -- integrating data (1/4)')
+      
+      immune.combined <- IntegrateData(anchorset = immune.anchors, dims = 1:(length(use.cols)-1))
+      DefaultAssay(immune.combined) <- "integrated"
   
   ### Re-construct Spectre object
-  
-  message(' -- constructing final data')
-  
-  ordr <- rbindlist(org.list)
-  
-  final <- as.data.table(t(immune.combined@assays$integrated@data))
-  final$CELLBARCODE <- ordr
-  
-  setorderv(final, 'CELLBARCODE')
-  final[['CELLBARCODE']] <- NULL
-  
-  names(final) <- paste0(names(final), '_rPCA_aligned')
-  
-  dat <- cbind(dat, final)
+      
+      message(' -- constructing final data (1/5)')
+      
+      ordr <- rbindlist(org.list)
+      
+      final <- as.data.table(t(immune.combined@assays$integrated@data))
+      final$CELLBARCODE <- ordr
+      
+      setorderv(final, 'CELLBARCODE')
+      final[['CELLBARCODE']] <- NULL
+      
+      final <- final[,c(match[[2]]), with = FALSE]
+      names(final) <- paste0(match[[1]], '_rPCA_aligned')
+      final
+
+      dat.start <- cbind(dat.start, final)
   
   ### Return
   
-  return(dat)
+  return(dat.start)
   
 }
 
