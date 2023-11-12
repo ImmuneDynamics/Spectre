71e1b86a037c1be9bc21335b976726d56ad4021e
Thomas Ashhurst <tomashhurst@users.noreply.github.com>
2023-11-12 18:07:25 +1100

    Update spectre.object.R

--- R/spectre.object.R
+++ R/spectre.object.R
@@ -10,60 +10,69 @@
 #                       key ='vector'
 #                     )
 
-Spectre <- setClass(Class = "Spectre",representation(data='list',
-                                      analysis="list",
-                                      other='list',
-                                      key='vector',
-                                      name='vector'))
+Spectre <- setClass(Class = "Spectre",representation('list',
+                                                     name='vector',
+                                                     spatial="list",
+                                                     other="list"
+))
 
-#' @import Spectre
-  
 setMethod("show",
           "Spectre",
           function(object){
-
-            ###
             
-                require('data.table')
+            ### Initial
+            
+            require('data.table')
+            
+            if(length(dat@name < 1)){
+              message(paste0("Spectre object: '", dat@name, "'"))
+            } else {
+              message("Spectre object")
+            }
+            
+            ### Datasets
+            
+            for(i in names(dat)){
+              r <- nrow(dat[[i]])
+              if(is.null(r)){
+                r <- 0
+              }
+              c <- ncol(dat[[i]])
+              if(is.null(c)){
+                c <- 0
+              }
+              message(paste0('  $', i, ' (', r, ' rows, ', c, ' cols)'))
+            }
+            
+            ### Spatial
+            
+            if(length(dat@spatial)){
+              message('  --')
+              
+              l <- length(names(dat@spatial))
 
-            ###
-               
-                if(length(object@name) > 0){
-                  message(' ')
-                  message("Spectre object (", object@name, ")")
-                } else {
-                  message(' ')
-                  message("Spectre object")
+              if(l <= 3){
+                for(i in names(dat@spatial)){
+                  message(paste0('  @spatial$', i))
                 }
-             
-            ### @data
-    
-                message("   @data")
-    
-                for(i in names(object@data)){
-                  # print(object@data[[i]])
-                 
-                  message(paste0("      $", i))
-                  
-                  message('          rows (n= ', formatC(nrow(object@data[[i]]), format = 'd', big.mark = ","), '): ', paste0(dat@data[[i]][[1]][1:5], ', '), ' ...')
-                  message('          cols (n= ', ncol(object@data[[i]]), '): ', paste0(names(dat@data[[i]])[2:6], ', '), ' ...')
+              } else {
+                for(i in c(1:3)){
+                  a <- names(dat@spatial)[i]
+                  message(paste0('  @spatial$', a))
                 }
-                
-            ### @analysis
-    
-                message("   @analysis")
-    
-                for(i in names(object@analysis)){
-                  message(paste0("     $", i))
-                }
-
-            ### @other
-                
-                message("   @other")
-    
-                for(i in names(object@other)){
-                  message(paste0("     $", i))
-                }
-
+                message(paste0('   +', l-3))
+              }
+            }
+            
+            ### Other
+            
+            if(length(dat@other)){
+              message('  --')
+              
+              for(i in names(dat@other)){
+                message(paste0('  @other$', i))
+              }
+            }
+            
           })
-                  
+
