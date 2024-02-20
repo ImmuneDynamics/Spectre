f678db91fe9e047e7de35c8cccb68ed774045ca6
Felix Marsh-Wakefield <50312652+Felixillion@users.noreply.github.com>
2024-02-21 09:51:00 +1100

    Updated test-make_volcano_plot and make.volcano.plot
    test-make_volcano_plot is now working. Updated make.volcano.plot to include example

--- tests/testthat/test-make_volcano_plot.R
+++ tests/testthat/test-make_volcano_plot.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("make volcano plot", {
+  # Create temporary directory where files will be saved
+  tmp.dir <- withr::with_tempdir(getwd(), 
+                                 clean = FALSE #leaves directory alone
+                                 )
+  setwd(tmp.dir)
+  
+  # Prepare data
+  sum.dat <- Spectre::demo.sum
+  to.plot <- names(sum.dat)[c(4:15)]
+  
+  # Calculate statistics
+  expect_no_error(
+    stat.dat <- create.stats(dat = sum.dat,
+                             use.cols = to.plot,
+                             sample.col = 'Sample',
+                             group.col = 'Group',
+                             comparisons = list(c('Mock', 'WNV'))
+    )
+  )
+  
+  # Create volcano plot
+  expect_no_error(
+    make.volcano.plot(dat.p = stat.dat[2,..to.plot],
+                      dat.fc = stat.dat[1,..to.plot],
+                      vars = to.plot,
+                      title = 'Volcano')
+  )
+  
+  # Test expected file was created (and is larger than 0 in size)
+  expect_true(
+    file.exists(grep("Volcano plot", list.files(), value = TRUE)) && 
+      file.size(grep("Volcano plot", list.files(), value = TRUE))>0
+  )
+  
 })
